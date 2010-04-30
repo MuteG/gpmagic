2119a412a98c869117a9056973e9cbe57a7dd54c
gaoyunpeng1982 <gaoyunpeng1982@3027dc2c-5d4b-6b70-68c4-67a1f4d952b8>
2010-04-30 09:20:46 +0000

    开始修改添加功能，界面调整，稀有度可以正确查询出来，所有的表实例将继承于接口以及抽象类。

---
+++ GPMagicBase/AbstractTableInstance.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GPSoft.GPMagic.GPMagicBase.SQLite;
+using GPSoft.GPMagic.GPMagicBase.Model;
+using System.Data;
+
+namespace GPSoft.GPMagic.GPMagicBase
+{
+    public abstract class AbstractTableInstance : ITableIntanceAction, IDisposable
+    {
+        private DatabaseOperator dbop;
+        private DataTable records = null;
+        protected string tableName = string.Empty;
+        /// <summary>
+        /// 获得全部记录数据
+        /// </summary>
+        public DataTable Records
+        {
+            get
+            {
+                if (null == records) this.Reload();
+                return records;
+            }
+        }
+        public AbstractTableInstance()
+        {
+            this.dbop = new DatabaseOperator(SQLiteDatabaseInformation.Connection);
+        }
+        private DataTable GetTableRecords()
+        {
+            DataTable result = null;
+            result = dbop.SelectTableData(this.tableName);
+            return result;
+        }
+        #region ITableIntanceAction 成员
+
+        /// <summary>
+        /// 再次读入数据
+        /// </summary>
+        public void Reload()
+        {
+            records = GetTableRecords();
+        }
+
+        /// <summary>
+        /// 保存数据
+        /// </summary>
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 在指定位置插入记录
+        /// </summary>
+        public void InsertAt()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 新建一条记录
+        /// </summary>
+        public void NewRecord()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 在尾部追加一条记录
+        /// </summary>
+        public void Append()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 删除一条数据
+        /// </summary>
+        public void Remove()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region IDisposable 成员
+
+        public void Dispose()
+        {
+            if (null != this.records) records.Dispose();
+            if (null != this.dbop) dbop.Dispose();
+        }
+
+        #endregion
+    }
+}
---
+++ GPMagicBase/ITableIntanceAction.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GPSoft.GPMagic.GPMagicBase
+{
+    public interface ITableIntanceAction
+    {
+        /// <summary>
+        /// 再次读入数据
+        /// </summary>
+        void Reload();
+
+        /// <summary>
+        /// 保存数据
+        /// </summary>
+        void Save();
+
+        /// <summary>
+        /// 在指定位置插入记录
+        /// </summary>
+        void InsertAt();
+
+        /// <summary>
+        /// 新建一条记录
+        /// </summary>
+        void NewRecord();
+
+        /// <summary>
+        /// 在尾部追加一条记录
+        /// </summary>
+        void Append();
+
+        /// <summary>
+        /// 删除一条数据
+        /// </summary>
+        void Remove();
+    }
+}
