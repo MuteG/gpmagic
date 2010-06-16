182c3cf93c4ccf92e0867a573313c6e965a8267a
gaoyunpeng1982@gmail.com <gaoyunpeng1982@gmail.com@3027dc2c-5d4b-6b70-68c4-67a1f4d952b8>
2010-06-16 13:05:36 +0000

    添加过滤功能

---
+++ trunk/GPSearch/Command/Filter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GPSoft.GPMagic.GPMagicBase.Model
+{
+    public class Filter
+    {
+        /// <summary>
+        /// 获取过滤字符串
+        /// </summary>
+        public string FilterString
+        {
+            get
+            {
+                return GetFilterString();
+            }
+        }
+
+        private string GetFilterString()
+        {
+            string result = string.Empty;
+            return result;
+        }
+
+        public void SetCardType()
+        {
+        }
+
+        public void SetKeyWord()
+        {
+        }
+
+        public void SetExpansions()
+        {
+        }
+    }
+}
---
+++ trunk/GPSearch/UI/FormFilterCardType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using GPSoft.GPMagic.GPMagicBase.UI;
+using GPSoft.GPMagic.GPMagicBase.Model;
+
+namespace GPSoft.GPMagic.GPSearch.UI
+{
+    public partial class FormFilterCardType : Form
+    {
+        public FormFilterCardType()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCardType_Click(object sender, EventArgs e)
+        {
+            tbxCardSubType.Text = CheckValuesDialog.Show(lblCardSubType.Text, string.Empty, "/", typeof(CardSubType));
+        }
+    }
+}
