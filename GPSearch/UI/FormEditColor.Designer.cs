2fa5792f2d9082192d5dc0ee7311faf46647a26a
gaoyunpeng1982 <gaoyunpeng1982@3027dc2c-5d4b-6b70-68c4-67a1f4d952b8>
2010-07-11 09:06:35 +0000

    继续完成各个窗口的设计（编辑颜色、编辑系列、过滤系列、编辑赛制）。

--- GPSearch/UI/FormEditColor.Designer.cs
+++ GPSearch/UI/FormEditColor.Designer.cs
@@ -28,32 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.components = new System.ComponentModel.Container();
+            this.lbxColorList = new System.Windows.Forms.ListBox();
+            this.lblColorCode = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblColorImage = new System.Windows.Forms.Label();
+            this.ofdColorImage = new System.Windows.Forms.OpenFileDialog();
+            this.tbxColorImage = new System.Windows.Forms.TextBox();
+            this.btnColorImageBrowser = new System.Windows.Forms.Button();
+            this.lblComment = new System.Windows.Forms.Label();
+            this.tbxComment = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
-            // listBox1
+            // lbxColorList
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 12;
-            this.listBox1.Location = new System.Drawing.Point(12, 12);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(133, 244);
-            this.listBox1.TabIndex = 0;
+            this.lbxColorList.FormattingEnabled = true;
+            this.lbxColorList.ItemHeight = 12;
+            this.lbxColorList.Location = new System.Drawing.Point(12, 12);
+            this.lbxColorList.Name = "lbxColorList";
+            this.lbxColorList.Size = new System.Drawing.Size(133, 184);
+            this.lbxColorList.TabIndex = 0;
+            // 
+            // lblColorCode
+            // 
+            this.lblColorCode.AutoSize = true;
+            this.lblColorCode.Location = new System.Drawing.Point(151, 15);
+            this.lblColorCode.Name = "lblColorCode";
+            this.lblColorCode.Size = new System.Drawing.Size(53, 12);
+            this.lblColorCode.TabIndex = 1;
+            this.lblColorCode.Text = "颜色代号";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(210, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(86, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // lblColorImage
+            // 
+            this.lblColorImage.AutoSize = true;
+            this.lblColorImage.Location = new System.Drawing.Point(151, 42);
+            this.lblColorImage.Name = "lblColorImage";
+            this.lblColorImage.Size = new System.Drawing.Size(53, 12);
+            this.lblColorImage.TabIndex = 3;
+            this.lblColorImage.Text = "颜色图片";
+            // 
+            // ofdColorImage
+            // 
+            this.ofdColorImage.FileName = "选择图片";
+            // 
+            // tbxColorImage
+            // 
+            this.tbxColorImage.Location = new System.Drawing.Point(210, 39);
+            this.tbxColorImage.Name = "tbxColorImage";
+            this.tbxColorImage.Size = new System.Drawing.Size(57, 21);
+            this.tbxColorImage.TabIndex = 4;
+            this.tbxColorImage.TextChanged += new System.EventHandler(this.tbxColorImage_TextChanged);
+            // 
+            // btnColorImageBrowser
+            // 
+            this.btnColorImageBrowser.Image = global::GPSoft.GPMagic.GPSearch.Properties.Resources.find;
+            this.btnColorImageBrowser.Location = new System.Drawing.Point(273, 38);
+            this.btnColorImageBrowser.Name = "btnColorImageBrowser";
+            this.btnColorImageBrowser.Size = new System.Drawing.Size(23, 23);
+            this.btnColorImageBrowser.TabIndex = 5;
+            this.toolTip1.SetToolTip(this.btnColorImageBrowser, "选择一个图片");
+            this.btnColorImageBrowser.UseVisualStyleBackColor = true;
+            // 
+            // lblComment
+            // 
+            this.lblComment.AutoSize = true;
+            this.lblComment.Location = new System.Drawing.Point(151, 69);
+            this.lblComment.Name = "lblComment";
+            this.lblComment.Size = new System.Drawing.Size(53, 12);
+            this.lblComment.TabIndex = 6;
+            this.lblComment.Text = "说明文字";
+            // 
+            // tbxComment
+            // 
+            this.tbxComment.Location = new System.Drawing.Point(153, 88);
+            this.tbxComment.Multiline = true;
+            this.tbxComment.Name = "tbxComment";
+            this.tbxComment.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbxComment.Size = new System.Drawing.Size(143, 79);
+            this.tbxComment.TabIndex = 7;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Image = global::GPSoft.GPMagic.GPSearch.Properties.Resources.Add;
+            this.btnAdd.Location = new System.Drawing.Point(153, 173);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(23, 23);
+            this.btnAdd.TabIndex = 5;
+            this.toolTip1.SetToolTip(this.btnAdd, "添加");
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Image = global::GPSoft.GPMagic.GPSearch.Properties.Resources.Delete;
+            this.btnDelete.Location = new System.Drawing.Point(182, 173);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(23, 23);
+            this.btnDelete.TabIndex = 5;
+            this.toolTip1.SetToolTip(this.btnDelete, "删除");
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Image = global::GPSoft.GPMagic.GPSearch.Properties.Resources.ok;
+            this.btnOK.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOK.Location = new System.Drawing.Point(240, 173);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(56, 23);
+            this.btnOK.TabIndex = 5;
+            this.btnOK.Text = "确认";
+            this.btnOK.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnOK.UseVisualStyleBackColor = true;
             // 
             // FormEditColor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 269);
-            this.Controls.Add(this.listBox1);
+            this.ClientSize = new System.Drawing.Size(309, 207);
+            this.Controls.Add(this.tbxComment);
+            this.Controls.Add(this.lblComment);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnColorImageBrowser);
+            this.Controls.Add(this.tbxColorImage);
+            this.Controls.Add(this.lblColorImage);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblColorCode);
+            this.Controls.Add(this.lbxColorList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "FormEditColor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "FormEditColor";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox lbxColorList;
+        private System.Windows.Forms.Label lblColorCode;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblColorImage;
+        private System.Windows.Forms.OpenFileDialog ofdColorImage;
+        private System.Windows.Forms.TextBox tbxColorImage;
+        private System.Windows.Forms.Button btnColorImageBrowser;
+        private System.Windows.Forms.Label lblComment;
+        private System.Windows.Forms.TextBox tbxComment;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
--- GPSearch/UI/FormFilterExpansions.Designer.cs
+++ GPSearch/UI/FormFilterExpansions.Designer.cs
@@ -28,19 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.lvwExpansionsList = new System.Windows.Forms.ListView();
+            this.cbxExpansions = new System.Windows.Forms.ComboBox();
+            this.lblExpansions = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOK.Image = global::GPSoft.GPMagic.GPSearch.Properties.Resources.ok;
+            this.btnOK.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOK.Location = new System.Drawing.Point(324, 223);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(56, 23);
+            this.btnOK.TabIndex = 18;
+            this.btnOK.Text = "确认";
+            this.btnOK.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Image = global::GPSoft.GPMagic.GPSearch.Properties.Resources.pencil_tool1;
+            this.btnEdit.Location = new System.Drawing.Point(203, 11);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(24, 23);
+            this.btnEdit.TabIndex = 17;
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Image = global::GPSoft.GPMagic.GPSearch.Properties.Resources.plus1;
+            this.btnNew.Location = new System.Drawing.Point(173, 11);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(24, 23);
+            this.btnNew.TabIndex = 16;
+            this.btnNew.UseVisualStyleBackColor = true;
+            // 
+            // lvwExpansionsList
+            // 
+            this.lvwExpansionsList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvwExpansionsList.CheckBoxes = true;
+            this.lvwExpansionsList.Location = new System.Drawing.Point(13, 40);
+            this.lvwExpansionsList.Name = "lvwExpansionsList";
+            this.lvwExpansionsList.Size = new System.Drawing.Size(367, 177);
+            this.lvwExpansionsList.TabIndex = 15;
+            this.lvwExpansionsList.UseCompatibleStateImageBehavior = false;
+            // 
+            // cbxExpansions
+            // 
+            this.cbxExpansions.FormattingEnabled = true;
+            this.cbxExpansions.Location = new System.Drawing.Point(46, 12);
+            this.cbxExpansions.Name = "cbxExpansions";
+            this.cbxExpansions.Size = new System.Drawing.Size(121, 20);
+            this.cbxExpansions.TabIndex = 14;
+            // 
+            // lblExpansions
+            // 
+            this.lblExpansions.AutoSize = true;
+            this.lblExpansions.Location = new System.Drawing.Point(11, 15);
+            this.lblExpansions.Name = "lblExpansions";
+            this.lblExpansions.Size = new System.Drawing.Size(29, 12);
+            this.lblExpansions.TabIndex = 13;
+            this.lblExpansions.Text = "系列";
             // 
             // FormFilterExpansions
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(554, 363);
+            this.ClientSize = new System.Drawing.Size(392, 258);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.lvwExpansionsList);
+            this.Controls.Add(this.cbxExpansions);
+            this.Controls.Add(this.lblExpansions);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "FormFilterExpansions";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "FormFilterExpansions";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.ListView lvwExpansionsList;
+        private System.Windows.Forms.ComboBox cbxExpansions;
+        private System.Windows.Forms.Label lblExpansions;
     }
 }
