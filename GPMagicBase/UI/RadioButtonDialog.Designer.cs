6048bd2386a78028bab6c1986058833289a8644b
gaoyunpeng1982 <gaoyunpeng1982@3027dc2c-5d4b-6b70-68c4-67a1f4d952b8>
2010-08-03 10:21:34 +0000

    添加单选项组控件（未测试、未添加迭代器实现）

---
+++ GPMagicBase/UI/RadioButtonDialog.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace GPSoft.GPMagic.GPMagicBase.UI
+{
+    partial class RadioButtonDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbxTitle = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.gbxTitle.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbxTitle
+            // 
+            this.gbxTitle.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbxTitle.AutoSize = true;
+            this.gbxTitle.Controls.Add(this.radioButton2);
+            this.gbxTitle.Controls.Add(this.radioButton1);
+            this.gbxTitle.Location = new System.Drawing.Point(12, 12);
+            this.gbxTitle.Name = "gbxTitle";
+            this.gbxTitle.Size = new System.Drawing.Size(249, 154);
+            this.gbxTitle.TabIndex = 0;
+            this.gbxTitle.TabStop = false;
+            this.gbxTitle.Text = "groupBox1";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(186, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(105, 172);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(30, 55);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(88, 16);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(30, 78);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(88, 16);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // RadioButtonDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(273, 207);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gbxTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(185, 140);
+            this.Name = "RadioButtonDialog";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "请选择一项";
+            this.gbxTitle.ResumeLayout(false);
+            this.gbxTitle.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxTitle;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
---
+++ Helper/Component/RadioGroup.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GPSoft.Helper.Component
+{
+    public partial class RadioGroup : UserControl
+    {
+        private RadioGroupItemCollection items = new RadioGroupItemCollection();
+
+        public RadioGroupItemCollection Items
+        {
+            get { return items; }
+            set { items = value; }
+        }
+
+        public int CheckedIndex
+        {
+            get
+            {
+                return GetCheckedIndex();
+            }
+        }
+
+        private int GetCheckedIndex()
+        {
+            int result = -1;
+            for (int i = 0; i < Items.Count; i++)
+            {
+                if (Items[i].Checked)
+                {
+                    result = i;
+                    break;
+                }
+            }
+            return result;
+        }
+        
+        public RadioGroup()
+        {
+            InitializeComponent();
+            items.groupPanel = this;
+        }
+    }
+
+    public class RadioGroupItemCollection
+    {
+        private List<RadioButton> listRadioButtonItems = new List<RadioButton>();
+        private Dictionary<string, int> dictRadioButtonItems = new Dictionary<string, int>();
+        internal RadioGroup groupPanel;
+
+        public int Count
+        {
+            get { return listRadioButtonItems.Count; }
+        }
+
+        public int Add(string itemText)
+        {
+            int result = -1;
+            if (Contains(itemText))
+            {
+                throw new Exception(string.Format("添加的项（{0}）已经存在。", itemText));
+            }
+            else
+            {
+                RadioButton newItem = NewItem(itemText);
+                result = listRadioButtonItems.Count;
+                listRadioButtonItems.Add(newItem);
+                dictRadioButtonItems.Add(itemText, result);
+                SetItemTop(listRadioButtonItems.Count - 1, newItem);
+                groupPanel.Controls.Add(newItem);
+            }
+            return result;
+        }
+
+        private void SetItemTop(int index, RadioButton item)
+        {
+            if (index == 0)
+            {
+                item.Top = 6;
+            }
+            else
+            {
+                RadioButton lastItem = listRadioButtonItems[index - 1];
+                item.Top = 6 + lastItem.Top + lastItem.Font.Height;
+            }
+        }
+
+        private RadioButton NewItem(string itemText)
+        {
+            RadioButton newItem = new RadioButton();
+            newItem.Text = itemText;
+            newItem.Font = groupPanel.Font;
+            newItem.Left = 12;
+            newItem.Visible = true;
+            return newItem;
+        }
+
+        public void Clear()
+        {
+            listRadioButtonItems.Clear();
+        }
+
+        public bool Contains(string itemText)
+        {
+            return dictRadioButtonItems.ContainsKey(itemText);
+        }
+
+        public int IndexOf(string itemText)
+        {
+            return Contains(itemText) ? dictRadioButtonItems[itemText] : -1;
+        }
+
+        public void Insert(int index, string itemText)
+        {
+            if (Contains(itemText))
+            {
+                throw new Exception(string.Format("插入的项（{0}）已经存在。", itemText));
+            }
+            else
+            {
+                RadioButton newItem = NewItem(itemText);
+                listRadioButtonItems.Insert(index, newItem);
+                ResetDictRadioButtonItems();
+                ResetRadioGroup();
+            }
+        }
+
+        private void ResetRadioGroup()
+        {
+            groupPanel.Controls.Clear();
+            for (int i = 0; i < listRadioButtonItems.Count; i++)
+            {
+                RadioButton item = listRadioButtonItems[i];
+                SetItemTop(i, item);
+                groupPanel.Controls.Add(item);
+            }
+        }
+
+        private void ResetDictRadioButtonItems()
+        {
+            for (int i = 0; i < listRadioButtonItems.Count; i++)
+            {
+                dictRadioButtonItems[listRadioButtonItems[i].Text] = i;
+            }
+        }
+
+        public void Remove(string itemText)
+        {
+            if (Contains(itemText))
+            {
+                RemoveAt(IndexOf(itemText));
+            }
+            else
+            {
+                throw new Exception(string.Format("没有找到指定的项（{0}）。", itemText));
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index > listRadioButtonItems.Count - 1)
+            {
+                throw new Exception(string.Format("索引值（{0}）超出允许的范围。", index));
+            }
+            else
+            {
+                listRadioButtonItems.RemoveAt(index);
+                ResetDictRadioButtonItems();
+                ResetRadioGroup();
+            }
+        }
+
+        public RadioButton this[int index]
+        {
+            get
+            {
+                return listRadioButtonItems[index];
+            }
+            set
+            {
+                dictRadioButtonItems.Remove(listRadioButtonItems[index].Text);
+                listRadioButtonItems[index] = value;
+                groupPanel.Controls[index].Text = value.Text;
+                dictRadioButtonItems.Add(value.Text, index);
+            }
+        }
+    }
+}
