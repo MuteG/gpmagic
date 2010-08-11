210b6f4b3dbe9457650d4290ca8e9f34c0d74d29
gaoyunpeng1982 <gaoyunpeng1982@3027dc2c-5d4b-6b70-68c4-67a1f4d952b8>
2010-08-11 09:58:33 +0000

    开始着手套牌保存/读取功能。

---
+++ GPMagicBase/Module/Deck/GPMagicDeck.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GPSoft.GPMagic.GPMagicBase.Module.Deck
+{
+    class GPMagicDeck:IDeck
+    {
+        #region IDeck 成员
+
+        public string Name
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Load(string deckFullPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(string deckFullPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ GPMagicBase/Module/Deck/IDeck.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GPSoft.GPMagic.GPMagicBase.Module.Deck
+{
+    public interface IDeck
+    {
+        string Name { set; get; }
+        string Description { set; get; }
+        void Load(string deckFullPath);
+        void Save(string deckFullPath);
+    }
+}
